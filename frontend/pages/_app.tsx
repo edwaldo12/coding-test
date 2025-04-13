389c529139affdf260a6383af6e4206e17e8f5af
Edwaldo Utama <53479749+edwaldo12@users.noreply.github.com>
2025-04-13 13:43:49 +0700

    component wrapper
    wrap all component to make background color better in reduce strain for eyes

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,14 @@
+import { Box } from '@mui/material';
 import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Component {...pageProps} />
-    </>
+    <Box
+      sx={{
+        backgroundColor: '#F8F8F8',
+      }}
+    >
+      <Component {...pageProps} />;
+    </Box>
   );
 }
